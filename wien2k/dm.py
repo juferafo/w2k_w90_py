7f32c31984da2de842473a83a959ac31831d0cff
juferafo <juferafo@hotmail.com>
2018-09-12 16:10:25 +0200

    dm.dmat_wien2kformat and dm.replace_dmat under development

--- wien2k/dm.py
+++ wien2k/dm.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 
 from __future__ import (absolute_import, division, print_function, unicode_literals)
+import os
 import numpy as np
 import quant.mathop as mathop
+import re
 
 dagger = mathop.dagger
 
@@ -22,7 +24,8 @@ class dmat(object):
         self.block = block
         
         if block == "full":
-            l = int((len(self.mat)/2 - 1 )/2)
+            l = int((len(self.mat)/2 - 1)/2)
+            print(l)
         else:
             l = int((len(self.mat) - 1 )/2)
         
@@ -33,15 +36,15 @@ class dmat(object):
 
 
     def matblock(self, b):
-        l = int((len(self.mat)-1)/2)
+        l = self.l
         if b == "UPUP":
-            return self.mat[0:l+1,0:l+1]
+            return self.mat[0:2*l+1,0:2*l+1]
         elif b == "UPDN":
-            return self.mat[0:l+1,l+1:]
+            return self.mat[0:2*l+1,2*l+1:]
         elif b == "DNUP":
-            return np.conj(self.mat[0:l+1,l+1:])
+            return np.conj(self.mat[0:2*l+1,2*l+1:])
         elif b == "DNDN":
-            return self.mat[l+1:,l+1:]
+            return self.mat[2*l+1:,2*l+1:]
         elif b == "full":
             return self.mat
         else:
@@ -114,7 +117,7 @@ def wrap_dmat(uu, dd, ud, transform_class = False):
     if transform_class:
         # If transform class == True it outputs the matrix as a dmat class as defined above
         density_matrix = np.bmat([[uu, ud], [dagger(ud), dd]])
-        density_matrix = dmat(density_matrix)
+        density_matrix = dmat(density_matrix, block = "full")
 
     else:
         density_matrix = np.bmat([[uu, ud], [dagger(ud), dd]]) 
@@ -122,5 +125,93 @@ def wrap_dmat(uu, dd, ud, transform_class = False):
     return density_matrix
 
 
-def replace_dmat():
+# UNDER DEVELOPMENT
+def dmat_wien2kformat(dm):
+    """ This method transform a given density matrix into lines written in wien2k dmat format """
+    if isinstance(dm, dmat):
+        dm = dm.mat
+
+    lines_output = []
+
+    vector_dm = dm.reshape(-1)
+    '''
+    for i in range(len(vector)):
+        dm_ij = ' % 1.12E' % dm[i].real + ' % 1.12E' % vector_dm[i].imag
+        line = dm_ij
+        if i%2 == 1:
+            
+        else:
+            line
+    '''
+    #return lines_output
     return None
+
+# UNDER DEVELOPMENT
+def replace_dmat(density_matrix, spin_block = 'UPUP', atom = None, fwrite = None):
+    '''
+    This function writes the provided density_matrix of the given atom into the file fwrite 
+    It only works for 1 or 2 atoms in the dmats!
+    The user MUST provide the full density matrix! (2l+1)*2!
+    '''
+    if not isinstance(density_matrix, dmat):
+        density_matrix = dmat(density_matrix, block = "full")
+    
+    dm = density_matrix.matblock(spin_block)
+    l  = density_matrix.l
+    n_lines_l = int((2*l+1)*((2*l+1)/2 + (2*l+1)%2))
+    
+    print(n_lines_l)
+    lines_dm = dmat_wien2kformat(dm)
+    print(len(lines_dm))
+    if fwrite and atom:
+        if os.path.exists(fwrite):
+            with open(fwrite, 'r') as fdm:
+                ldm = fdm.readlines()
+            
+            re_at = re.compile(str(atom)+'\s(atom)\s(density)\s(matrix)')
+            for i in range(len(ldm)):
+                if re_at.search(ldm[i]):
+                    i_atom = i
+                    break
+            
+            with open(fwrite, 'w') as fdm_out:
+                for i in range(len(ldm)):
+                    if i in range(i_atom + 2, i_atom + n_lines_l):
+                        fdm_out.write("\n")
+                    else:
+                        fdm_out.write(ldm[i])
+                
+                
+                
+                '''
+                for j in range(len(ldm)):
+                    if j == i_atom:
+                        fdm_out.write(ldm[j])
+                        fdm_out.write(ldm[j+1])
+                        break
+                    else:
+                        fdm_out.write(ldm[j])
+                               print(j+1+k)
+                print(len(ldm))
+
+                for u in range(j+k, len(ldm)):
+                    fdm_out.write(ldm[u])
+                '''
+            '''
+            with open(fwrite, 'w') as fdm_out:
+                for j in range(0,i):
+                    fdm_out.write(ldm[j])
+            '''
+
+        else:
+            print("ERROR: "+fwrite+" file does not exist")
+            return None
+        
+
+    else:
+        if not fwrite:
+            print("ERROR! No fwrite provided!")
+        else:
+            print("ERROR! No atom provided!")
+
+        return None
