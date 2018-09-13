73799ea091fc575aef8df0ba9b456e092c24978c
juferafo <juferafo@hotmail.com>
2018-09-13 12:32:09 +0200

    dm.dmat_wien2kformat and dm.replace_dmat finnished and tested

--- wien2k/dm.py
+++ wien2k/dm.py
@@ -25,7 +25,6 @@ class dmat(object):
         
         if block == "full":
             l = int((len(self.mat)/2 - 1)/2)
-            print(l)
         else:
             l = int((len(self.mat) - 1 )/2)
         
@@ -128,41 +127,52 @@ def wrap_dmat(uu, dd, ud, transform_class = False):
 # UNDER DEVELOPMENT
 def dmat_wien2kformat(dm):
     """ This method transform a given density matrix into lines written in wien2k dmat format """
+    print(type(dm)) 
     if isinstance(dm, dmat):
         dm = dm.mat
+        l  = dmat.l
+    else:
+
+        l = int((len(dm) - 1)/2) 
 
     lines_output = []
 
-    vector_dm = dm.reshape(-1)
-    '''
-    for i in range(len(vector)):
-        dm_ij = ' % 1.12E' % dm[i].real + ' % 1.12E' % vector_dm[i].imag
-        line = dm_ij
-        if i%2 == 1:
-            
-        else:
-            line
-    '''
-    #return lines_output
-    return None
+    for line_dmat in dm:
+        c_i    = False
+        for j in range(len(line_dmat)):
+            dm_ij = ' % 1.12E' % line_dmat[j].real + ' % 1.12E' % line_dmat[j].imag
+            if c_i:
+                line = line + dm_ij + "\n"
+                lines_output.append(line)
+            else:
+                if j == (len(dm) - 1):
+                    line = dm_ij + '\n'
+                    lines_output.append(line)
+                else:
+                    line = dm_ij + '  '
+            c_i = not c_i
+
+    return lines_output
+
 
 # UNDER DEVELOPMENT
 def replace_dmat(density_matrix, spin_block = 'UPUP', atom = None, fwrite = None):
     '''
     This function writes the provided density_matrix of the given atom into the file fwrite 
-    It only works for 1 or 2 atoms in the dmats!
+    It was only tested for 1 or 2 atoms in the dmats!
     The user MUST provide the full density matrix! (2l+1)*2!
     '''
+    dmat_spin = {'UPUP' : 'up', 'DNDN' : 'dn', 'UPDN' : 'ud'}
+
+    if spin_block == 'DNUP':
+        print("DNUP block not available for case.dmat files.")
+        print("Please write UPDN sector instead.")
+
     if not isinstance(density_matrix, dmat):
         density_matrix = dmat(density_matrix, block = "full")
     
     dm = density_matrix.matblock(spin_block)
-    l  = density_matrix.l
-    n_lines_l = int((2*l+1)*((2*l+1)/2 + (2*l+1)%2))
-    
-    print(n_lines_l)
     lines_dm = dmat_wien2kformat(dm)
-    print(len(lines_dm))
     if fwrite and atom:
         if os.path.exists(fwrite):
             with open(fwrite, 'r') as fdm:
@@ -174,44 +184,18 @@ def replace_dmat(density_matrix, spin_block = 'UPUP', atom = None, fwrite = None
                     i_atom = i
                     break
             
+            ldm[i_atom+2:i_atom+2+len(lines_dm)] = lines_dm
+            
             with open(fwrite, 'w') as fdm_out:
-                for i in range(len(ldm)):
-                    if i in range(i_atom + 2, i_atom + n_lines_l):
-                        fdm_out.write("\n")
-                    else:
-                        fdm_out.write(ldm[i])
+                for l in ldm:
+                    fdm_out.write(l)
                 
-                
-                
-                '''
-                for j in range(len(ldm)):
-                    if j == i_atom:
-                        fdm_out.write(ldm[j])
-                        fdm_out.write(ldm[j+1])
-                        break
-                    else:
-                        fdm_out.write(ldm[j])
-                               print(j+1+k)
-                print(len(ldm))
-
-                for u in range(j+k, len(ldm)):
-                    fdm_out.write(ldm[u])
-                '''
-            '''
-            with open(fwrite, 'w') as fdm_out:
-                for j in range(0,i):
-                    fdm_out.write(ldm[j])
-            '''
-
         else:
             print("ERROR: "+fwrite+" file does not exist")
             return None
-        
 
     else:
         if not fwrite:
             print("ERROR! No fwrite provided!")
         else:
             print("ERROR! No atom provided!")
-
-        return None
